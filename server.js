14c817225aa033f082674ca39b748e23ed728944
rjairath <rishabhjairath@gmail.com>
2019-05-26 00:31:44 +0530

    weather api called

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
+
 const app = express();
 const port = 3000;
+const apiKey = process.env.WEATHER_API_KEY;
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -14,8 +17,24 @@ app.get('/', (req, res) =>{
 app.post('/', (req, res)=>{
     // res.render('index');
     // console.log(req.body.city);
-    // Make the api call here
-    
+    // Make the API call here
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    request(url, function(err, response, body){
+        if(err){
+            res.render('index', {weather: null, error: "Error, Please try again"});
+        }
+        else{
+            let weather = JSON.parse(body);
+            if(weather.main == undefined){
+                res.render('index', {weather: null, error: "city not found"});
+            }else{
+                let weatherText = `It's ${weather.main.temp} in ${weather.name}`;
+                res.render('index', {weather: weatherText, error: null});
+            }
+        }
+    });
 })
 
 app.listen(port, () => {
