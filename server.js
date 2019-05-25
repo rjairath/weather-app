be3d9a641e0a643613256e52d2f6ead91ebd7da9
rjairath <rishabhjairath@gmail.com>
2019-05-26 00:01:23 +0530

    css error fixed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-app.use(express.static('/public'));
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
