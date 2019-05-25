66b21821fe0fed384091f01b3713db32005c45f3
rjairath <rishabhjairath@gmail.com>
2019-05-25 22:07:02 +0530

    basic structure

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(express.static('/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) =>{
+    res.render('index');
+});
+app.post('/', (req, res)=>{
+    // res.render('index');
+    // console.log(req.body.city);
+    // Make the api call here
+    
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
